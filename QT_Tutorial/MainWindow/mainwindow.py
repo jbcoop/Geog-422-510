7a03380fa181c5fd005e915fd65efd10183f28be
Coop <jbcoop2@hotmail.com>
2025-01-08 00:10:54 -0600

    jan07updates

--- QT_Tutorial/MainWindow/mainwindow.py
+++ QT_Tutorial/MainWindow/mainwindow.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QMainWindow, QToolBar
 
 class MainWindow(QMainWindow):
     def __init__(self,app):
@@ -23,6 +25,18 @@ class MainWindow(QMainWindow):
         menu_bar.addMenu("Setting")
         menu_bar.addMenu("Help")
 
+        toolbar = QToolBar("Main Toolbar")
+        toolbar.setIconSize(QSize(16,16))
+        self.addToolBar(toolbar)
+        toolbar.addAction(quit_action)
+
+        action1 = QAction("Some Action", self)
+        action1.setStatusTip("This is some action")
+        action1.triggered.connect(self.tool_button_click)
+        toolbar.addAction(action1)
+
+    def tool_button_click(self):
+        print("Tool button clicked")
 
     def quit_app(self):
         self.app.quit()
